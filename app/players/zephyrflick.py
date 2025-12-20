a11490607397d2d772d95a3b15129d7e5ad3bfe6
skoruppa <skoruppa@gmail.com>
2025-12-20 03:58:12 +0100

    try without headers

--- app/players/zephyrflick.py
+++ app/players/zephyrflick.py
@@ -41,12 +41,13 @@ async def get_video_from_zephyrflick_player(player_url: str):
         
         # Rewrite URL to use our proxy
         video_url = video_url.replace('https://play.zephyrflick.top', f'{Config.PROTOCOL}://{Config.REDIRECT_URL}')
-        
+
+        stream_headers = None
         # Headers for stream (without X-Requested-With)
-        stream_headers = {
-            'User-Agent': get_random_agent(),
-            'Referer': player_url
-        }
+        # stream_headers = {
+        #     'User-Agent': get_random_agent(),
+        #     'Referer': player_url
+        # }
         
         # Get subtitles from player page
         subtitles = []
